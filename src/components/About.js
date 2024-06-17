88386bf64ac08e7613d342b02026c8f20a09f687
Zhouyirui <72332263+Zhouyirui@users.noreply.github.com>
2024-06-18 02:57:20 +0800

    style About page

--- src/components/About.js
+++ src/components/About.js
@@ -1,13 +1,18 @@
-import React from 'react';
-import './About.css';
+import React from "react";
+import "./About.css";
 
 const About = () => {
   return (
     <section className="about" id="about">
-      <h2>About</h2>
-      <p>Hi, I've been working at the intersection of UX, Service Design and Conversion Design for over a decade. And I believe that adopting a <strong>user-centred approach</strong> in design is critical in crafting a holistic brand experience to drive customer LTV for the business.</p>
+      <div className="title">About</div>
+      <p>
+        Hi, I've been working at the intersection of UX, Service Design and
+        Conversion Design for over a decade. And I believe that adopting a{" "}
+        <strong>user-centred approach</strong> in design is critical in crafting
+        a holistic brand experience to drive customer LTV for the business.
+      </p>
     </section>
   );
-}
+};
 
 export default About;
