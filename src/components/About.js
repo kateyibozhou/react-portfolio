2d5dd21bbd799c3ab58eea7da3b1bc94b904b97b
Zhouyirui <72332263+Zhouyirui@users.noreply.github.com>
2024-06-18 22:21:36 +0800

    Change font size of text

--- src/components/About.js
+++ src/components/About.js
@@ -1,4 +1,5 @@
 import React from "react";
+
 import "./About.css";
 
 const About = () => {
@@ -6,10 +7,12 @@ const About = () => {
     <section className="about" id="about">
       <div className="title">About</div>
       <p>
-        Hi, I've been working at the intersection of UX, Service Design and
-        Conversion Design for over a decade. And I believe that adopting a{" "}
+        I've been working at the intersection of{" "}
+        <strong>UX, Service Design and Conversion Design </strong> for over a
+        decade. And I believe that adopting a{" "}
         <strong>user-centred approach</strong> in design is critical in crafting
-        a holistic brand experience to drive customer LTV for the business.
+        a holistic brand experience to{" "}
+        <strong>drive customer LTV for the business </strong>.
       </p>
     </section>
   );
