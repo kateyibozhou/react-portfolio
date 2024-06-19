257558f9c6288a4c3278c315e9d2d7e13a5a6a64
Zhouyirui <72332263+Zhouyirui@users.noreply.github.com>
2024-06-19 18:47:22 +0800

    Style other skills

--- src/components/CoreSkills.js
+++ src/components/CoreSkills.js
@@ -1,7 +1,7 @@
 import React from "react";
 import SkillCard from "./SkillCard";
-import { ReactComponent as growthLogo } from "../assets/growth.svg";
 import { ReactComponent as SDLogo } from "../assets/SD.svg";
+import { ReactComponent as GrowthLogo } from "../assets/growth.svg";
 import { ReactComponent as UXLogo } from "../assets/UX.svg";
 
 import "./CoreSkills.css";
@@ -12,27 +12,25 @@ const CoreSkills = () => {
       <div className="title">Core Skills</div>
       <div className="skills-table">
         <SkillCard
-          icons={<SDLogo />}
+          icon={<UXLogo />}
           title="User Experience"
           description="Advocating for end users is just as important as designing for business needs."
         />
         <SkillCard
-          icon={<growthLogo />}
+          icon={<GrowthLogo />}
           title="Growth Design"
           description="I make sure that design solutions always fulfil business needs, and lead to optimisation, retention and conversion."
         />
         <SkillCard
           icon={<SDLogo />}
           title="Service Design"
-          description="I take an ecosystem view of the product and understand the user journey in my design processes."
+          description="I take an ecosystems view of the product and understand the user journey in my design processes."
         />
       </div>
-      <p>
-        Other skills I am fluent in:{" "}
-        <strong>
-          Branding & Packaging / Illustration / Ecommerce Campaign
-        </strong>
-      </p>
+      <div>
+        <p>Other skills I am fluent in:</p>
+        <div className="other-skills">Branding & Packaging / Illustration</div>
+      </div>
     </section>
   );
 };
