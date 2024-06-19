41b29d66490fad71834fb18a8f91ebfbf3fc129b
Zhouyirui <72332263+Zhouyirui@users.noreply.github.com>
2024-06-19 12:04:14 +0800

    Add core skills icons and style card

--- src/components/CoreSkills.js
+++ src/components/CoreSkills.js
@@ -1,19 +1,40 @@
-import React from 'react';
-import SkillCard from './SkillCard';
-import './CoreSkills.css';
+import React from "react";
+import SkillCard from "./SkillCard";
+import { ReactComponent as growthLogo } from "../assets/growth.svg";
+import { ReactComponent as SDLogo } from "../assets/SD.svg";
+import { ReactComponent as UXLogo } from "../assets/UX.svg";
+
+import "./CoreSkills.css";
 
 const CoreSkills = () => {
   return (
     <section className="core-skills">
-      <h2>Core skills</h2>
-      <div className="skills">
-        <SkillCard title="User Experience" description="Advocating for end users is just as important as designing for business needs." />
-        <SkillCard title="Growth Design" description="I make sure that design solutions always fulfil business needs, and lead to optimisation, retention and conversion." />
-        <SkillCard title="Service Design" description="I take an ecosystem view of the product and understand the user journey in my design processes." />
+      <div className="title">Core Skills</div>
+      <div className="skills-table">
+        <SkillCard
+          icons={<SDLogo />}
+          title="User Experience"
+          description="Advocating for end users is just as important as designing for business needs."
+        />
+        <SkillCard
+          icon={<growthLogo />}
+          title="Growth Design"
+          description="I make sure that design solutions always fulfil business needs, and lead to optimisation, retention and conversion."
+        />
+        <SkillCard
+          icon={<SDLogo />}
+          title="Service Design"
+          description="I take an ecosystem view of the product and understand the user journey in my design processes."
+        />
       </div>
-      <p>Other skills I am fluent in: <strong>Branding & Packaging / Illustration / Ecommerce Campaign</strong></p>
+      <p>
+        Other skills I am fluent in:{" "}
+        <strong>
+          Branding & Packaging / Illustration / Ecommerce Campaign
+        </strong>
+      </p>
     </section>
   );
-}
+};
 
 export default CoreSkills;
