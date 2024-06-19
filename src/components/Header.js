5d51f461e26b0c1d2c4c507fdc6848bb336a871a
Priyank🌸 <p_chaudhary@apple.com>
2024-06-20 00:05:22 +0800

    resume

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,7 +9,7 @@ const Header = () => {
         <ul>
           <li><a href="#projects">Projects</a></li>
           <li><a href="#about">About</a></li>
-          <li><a href="#resume">Resume</a></li>
+          <li><a href="/ZhouYiboResume.pdf" target="_blank" rel="noopener noreferrer">Resume</a></li>
           <li><a href="#contact">Contact</a></li>
         </ul>
       </nav>
