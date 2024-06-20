4727a863f30312e787f393daa6361723e9e583a2
Priyank🌸 <p_chaudhary@apple.com>
2024-06-20 19:26:54 +0800

    refactored to have separate homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from "./components/Header"; 
-import LandingPage from "./components/LandingPage";
-import About from "./components/About";
-import CoreSkills from "./components/CoreSkills";
-import SelectedWork from "./components/SelectedWork";
 import CaseStudy from "./components/CaseStudy";
 import Projects from "./components/Projects";
 import Footer from "./components/Footer";
+import HomePage from "./pages/HomePage/HomePage";
 import "./App.css";
 
 function App() {
@@ -17,18 +14,8 @@ function App() {
       <div className="App">
         <Header />
         <Routes>
-          <Route
-            path="/"
-            element={
-              <>
-                <LandingPage />
-                <About />
-                <CoreSkills />
-                <SelectedWork />
-              </>
-            }
-          />
           <Route path="/projects" element={<Projects />} />
+          <Route path="/" element={<HomePage />} />
           <Route path="/case-study/:id" element={<CaseStudy />} />
         </Routes>
         <Footer />
