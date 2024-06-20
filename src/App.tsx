e1f3b2f4a8db47537c996228d45e189cbdc96603
Priyank🌸 <p_chaudhary@apple.com>
2024-06-21 00:40:41 +0800

    use a helmet

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,41 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Header from "./components/Header"; 
+import { ThemeProvider } from 'styled-components';
+import { Helmet, HelmetProvider } from 'react-helmet-async';
+import GlobalStyle from './GlobalStyle';
+import theme from './theme';
+import Header from "./components/Header";
 import CaseStudy from "./components/CaseStudy";
 import Projects from "./components/Projects";
 import Footer from "./components/Footer";
 import HomePage from "./pages/HomePage/HomePage";
-import "./App.css";
+import './App.css';
 
 function App() {
   document.title = "Yibo Zhou's Portfolio";
   return (
-    <Router>
-      <div className="App">
-        <Header />
-        <Routes>
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/" element={<HomePage />} />
-          <Route path="/case-study/:id" element={<CaseStudy />} />
-        </Routes>
-        <Footer />
-      </div>
-    </Router>
+    <ThemeProvider theme={theme}>
+      <HelmetProvider>
+        <Helmet>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Work+Sans&family=Fraunces:wght@100..900&display=swap"
+            rel="stylesheet"
+          />
+        </Helmet>
+        <GlobalStyle />
+        <Router>
+          <div className="App">
+            <Header />
+            <Routes>
+              <Route path="/projects" element={<Projects />} />
+              <Route path="/" element={<HomePage />} />
+              <Route path="/case-study/:id" element={<CaseStudy />} />
+            </Routes>
+            <Footer />
+          </div>
+        </Router>
+      </HelmetProvider>
+    </ThemeProvider>
   );
 }
 
