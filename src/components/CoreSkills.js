b4b930dcce09e8740efe800d5e20062c3db82b85
Zhouyirui <72332263+Zhouyirui@users.noreply.github.com>
2024-06-22 15:02:38 +0800

    update description of core skills and arrangement

--- src/components/CoreSkills.js
+++ src/components/CoreSkills.js
@@ -14,17 +14,17 @@ const CoreSkills = () => {
         <SkillCard
           icon={<UXLogo />}
           title="User Experience"
-          description="Advocating for end users is just as important as designing for business needs."
+          description="Advocating for end users is just as important as designing for business needs. I strive to design products and experiences that are delightful and easy to use."
+        />
+        <SkillCard
+          icon={<SDLogo />}
+          title="Service Design"
+          description="I take an ecosystems view of the product and understand the user journey in my design processes. I make sure I’m solving the right problems before delving into the solutions."
         />
         <SkillCard
           icon={<GrowthLogo />}
           title="Growth Design"
           description="I make sure that design solutions always fulfil business needs, and lead to optimisation, retention and conversion."
-        />
-        <SkillCard
-          icon={<SDLogo />}
-          title="Service Design"
-          description="I take an ecosystems view of the product and understand the user journey in my design processes."
         />
       </div>
       <div>
