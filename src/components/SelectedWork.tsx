a7dd650b7f1b3890cbd82fc12d6fe66b9a973108
Priyank🌸 <p_chaudhary@apple.com>
2024-07-13 00:05:51 +0800

    image not loading

--- src/components/SelectedWork.tsx
+++ src/components/SelectedWork.tsx
@@ -12,6 +12,7 @@ const SelectedWork = () => {
       description:
         "Increased renewal revenue by 83% within 7 days of testing by triangulating multiple points of data to provide a user friendly design recommendation.",
       tags: [Tags.UX, Tags.growth],
+      image_path: "/product-ux/evpn-increase-user-renewal-rate/desktop/hero.png",
       path: "/increase-user-renewal-rate",
     },
     {
@@ -19,6 +20,7 @@ const SelectedWork = () => {
       description:
         "Created 8% uplift in average order value (AOV) of the Regular mobile checkout page design by understanding the user journey.",
       tags: [Tags.UX, Tags.growth],
+      image_path: "/product-ux/evpn-increase-user-renewal-rate/desktop/hero.png",
       path: "/optimise-checkout-page",
     },
     {
@@ -26,6 +28,7 @@ const SelectedWork = () => {
       description:
         "Designed an easy to use ExpressVPN B2B admin panel as an MVP while navigating ambiguities and tight timelines.",
       tags: [Tags.UX],
+      image_path: "/product-ux/evpn-increase-user-renewal-rate/desktop/hero.png",
       path: "/enterprise-dashboard-mvp",
     },
     {
@@ -33,6 +36,7 @@ const SelectedWork = () => {
       description:
         "Collaborated with user researchers to adapt Shell App for local markets and introduced a new payment model to users.",
       tags: [Tags.UX],
+      image_path: "/product-ux/evpn-increase-user-renewal-rate/desktop/hero.png",
       path: "/shell-mobile-app",
     },
     {
@@ -40,6 +44,7 @@ const SelectedWork = () => {
       description:
         "Designed a workshop to develop an omni-channel campaign strategy to help Philips Air purifier enter into 2 new B2B markets.",
       tags: [Tags.strategy, Tags.SD],
+      image_path: "/product-ux/evpn-increase-user-renewal-rate/desktop/hero.png",
       path: "/philips-gtm-launch-campaign",
     },
     {
@@ -47,6 +52,7 @@ const SelectedWork = () => {
       description:
         "Improve work efficiency and understanding of customer experiences through building a single source of truth.",
       tags: [Tags.SD],
+      image_path: "/product-ux/evpn-increase-user-renewal-rate/desktop/hero.png",
       path: "/customer-journey-maps",
     },
   ];
@@ -61,6 +67,7 @@ const SelectedWork = () => {
             description={project.description}
             tags={project.tags}
             path={project.path}
+            image_path={project.image_path}
           />
         ))}
       </div>
--- src/components/WorkCard.js
+++ src/components/WorkCard.js
@@ -5,18 +5,27 @@ import { TagColors } from "../utils/SelectedWorkTags";
 
 import "./WorkCard.css";
 
-const WorkCard = ({ title, description, tags, path }) => {
+const WorkCard = ({ title, description, tags, path, image_path }) => {
   const navigate = useNavigate();
   const handleClick = () => {
     navigate(path);
   };
   return (
-    <div className="work-card" onClick={handleClick}>
+    <div
+      className="work-card"
+      onClick={handleClick}
+      style={{
+        backgroundImage: require(`../assets/images/case-studies/product-ux/evpn-increase-user-renewal-rate/desktop/hero.png`),
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat'
+      }}
+    >
       <div className="name">{title}</div>
       <div className="description">{description}</div>
       <div className="tags">
         {tags?.map((t) => (
-          <Tag style={{ color: "black" }} bordered={false} color={TagColors[t]}>
+          <Tag key={t} style={{ color: "black" }} bordered={false} color={TagColors[t]}>
             {t}
           </Tag>
         ))}
