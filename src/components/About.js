f69c9e5254fcf3185edb3e91ab7b2e10e277b512
Priyank🌸 <p_chaudhary@apple.com>
2024-07-12 13:59:09 +0800

    font size

--- src/components/About.js
+++ src/components/About.js
@@ -1,18 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 import "./About.css";
 
 const About = () => {
+  useEffect(() => {
+    AOS.init();
+  }, []); // The empty array ensures this effect runs only once after the initial render
+
   return (
     <section className="about" id="about">
-      <div className="title">About</div>
       <div className="description">
+        <div data-aos="fade-up-left">
         I've been working at the intersection of{" "}
-        <strong>UX, Service Design and Conversion Design </strong> for over a
-        decade. And I believe that adopting a{" "}
-        <strong>user-centred approach</strong> in design is critical in crafting
+        UX, Service Design and Conversion Design for over a
+        decade.
+        </div>
+        <div data-aos="fade-up-right">
+        I believe that adopting a{" "}
+        user-centred approach in design is critical in crafting
         a holistic brand experience to{" "}
-        <strong>drive customer LTV for the business </strong>.
+        drive customer LTV for the business.
+        </div>
       </div>
     </section>
   );
