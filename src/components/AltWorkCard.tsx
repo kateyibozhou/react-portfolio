d882f120de7bdd7b28187cf0e2828a6e9a371479
Priyank🌸 <p_chaudhary@apple.com>
2024-07-25 14:17:17 +0800

    parity with landing page

--- src/components/AltWorkCard.tsx
+++ src/components/AltWorkCard.tsx
@@ -1,15 +1,14 @@
+/* TODO: Merge this and WorkCard.js implementation */
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 
 const CardContainer = styled.div`
-  border: 1px solid ${({ theme }) => theme.colors.primaryDark};
   background-color: ${({ theme }) => theme.colors.primaryLight};
   color: ${({ theme }) => theme.colors.workCardDarkText};
   text-align: left;
-  border-radius: 0.8rem;
+  border-radius: 1rem;
   box-shadow: 0.1rem 0.1rem 0.1rem rgba(0, 0, 0, 0.2);
-  padding: 1rem;
   cursor: pointer;
   transition: transform 0.3s ease-in-out;
 
@@ -21,19 +20,41 @@ const CardContainer = styled.div`
 
 const CardTitle = styled.h4`
   font-family: ${({ theme }) => theme.fonts.primary};
+  padding-left: 1.25rem;
+  padding-right: 1.25rem;
+  font-size: 1.5rem;
+  line-height: 1.3;
+  margin-bottom: 0.25rem;
+`;
+
+const CardImageContainer = styled.div<{ image_path: string }>`
+  background-image: url(${(props) => props.image_path});
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  height: 24rem;
+  border-radius: 1rem 1rem 0 0;
+
+  @media (max-width: 50rem) {
+    height: 20rem;
+  }
 `;
 
 const CardDescription = styled.p`
-  font-size: 0.5rem;
+  font-size: 1rem;
+  padding-left: 1.25rem;
+  padding-right: 1.25rem;
+  padding-bottom: 1rem;
 `;
 
 interface WorkCardProps {
   title: string;
   description: string;
   path: string;
+  image_path: string;
 }
 
-const WorkCard: React.FC<WorkCardProps> = ({ title, description, path }) => {
+const WorkCard: React.FC<WorkCardProps> = ({ title, description, path, image_path }) => {
   const navigate = useNavigate();
 
   const handleClick = () => {
@@ -42,6 +63,7 @@ const WorkCard: React.FC<WorkCardProps> = ({ title, description, path }) => {
 
   return (
     <CardContainer onClick={handleClick}>
+      <CardImageContainer image_path={image_path} />
       <CardTitle>{title}</CardTitle>
       <CardDescription>{description}</CardDescription>
     </CardContainer>
