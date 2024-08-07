dbccb915006af06d267a6546f5c4a8e21f9a3b12
Priyank🌸 <p_chaudhary@apple.com>
2024-08-07 16:53:51 +0800

    browser router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 import { Helmet, HelmetProvider } from 'react-helmet-async';
 import GlobalStyle from './GlobalStyle';
