8c2089498d34f0e19810057b0b93f121ab8735b6
Priyank🌸 <p_chaudhary@apple.com>
2024-08-07 16:53:08 +0800

    correct contact

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,9 +6,9 @@ const Footer = () => {
     <footer className="footer">
       <h2>Let's collaborate!</h2>
       <div className="contacts">
-        <a href="https://www.linkedin.com" target="_blank" rel="noopener noreferrer">LinkedIn</a>
-        <a href="mailto:your-email@example.com">E-mail</a>
-        <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">Instagram</a>
+        <a href="https://www.linkedin.com/yibozhoukate" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+        <a href="mailto:kateyibozhou@gmail.com">E-mail</a>
+        <a href="https://www.instagram.com/kateyibo" target="_blank" rel="noopener noreferrer">Instagram</a>
       </div>
     </footer>
   );
